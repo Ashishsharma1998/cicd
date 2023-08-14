1223d1aaf97c69efba6b312ec164824bd6d056ca
Ashish sharma <sharma07101998@gmail.com>
2023-08-14 23:58:10 +0530

    second commit

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const { PORT } = require("./config/serverConfig");
-const userRoutes = require("./routes/userRoute");
+const userRoute = require("./routes/userRoute");
 const connectDB = require("./config/databaseConfig");
 const app = express();
 
 app.use(express.json());
 
-app.use("/api", userRoutes);
+app.use("/api", userRoute);
 
 app.listen(PORT, async () => {
   console.log(`server started at port ${PORT}`);
