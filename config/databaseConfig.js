a608e11cfa951aa48840ec8e9c1bfacd30c80242
Ashish Sharma <76773930+Ashishsharma1998@users.noreply.github.com>
2023-08-16 21:56:28 +0530

    Delete config directory

--- config/databaseConfig.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const connectDB = async () => {
-  const url =
-    "mongodb+srv://vincenzo:test123456@cluster0.mhjv9.mongodb.net/cicdDB?retryWrites=true&w=majority" ||
-    process.env.MONGO_URL;
-  await mongoose.connect(url);
-};
-
-module.exports = connectDB;
